ca2bbb5f4a8ca4d1e68cd9cd64d357fe2cd64c67
Will Arnuk <warnuk@umich.edu>
2023-05-08 15:55:28 -0400

    Add analysis_tools
    Includes functions for finding peaks and troughs in univariate/bivariate data

---
+++ R/analysis_tools.R
@@ -0,0 +1,41 @@
+#' Locate peaks
+#'
+#' @param y Input vector of data in which to find peaks.
+#' @param x (optional) locations of data observations.
+#' @param threshold An optional value to be used as a cut-off for peaks.
+#' @return A data.frame object with peak locations (x) and peak values (y).
+#' @examples
+#' x <- seq(0, 2*pi, 1000)
+#' y <- sin(2*x)
+#' find_peaks(y, x)
+#' @export
+find_peaks <- function(y, x=seq(1,length(y)), threshold=NULL) {
+  peaks <- x[seq(1, length(x))[which(diff(sign(diff(y)))==-2)]+1]
+  peaks_y <- y[!is.na(match(x, peaks))]
+  if (!is.null(threshold)) {
+    peaks <- peaks[peaks_y >= threshold]
+    peaks_y <- peaks_y[peaks_y >= threshold]
+  }
+  return(data.frame(x=peaks, y=peaks_y))
+}
+
+#' Locate troughs
+#'
+#' @param y Input vector of data in which to find troughs.
+#' @param x (optional) locations of data observations.
+#' @param threshold An optional value to be used as a cut-off for troughs.
+#' @return A data.frame object with trough locations (x) and troughs value (y).
+#' @examples
+#' x <- seq(0, 2*pi, 1000)
+#' y <- sin(2*x)
+#' find_troughs(y, x)
+#' @export
+find_troughs <- function(y, x=seq(1,length(y)), threshold=NULL) {
+  troughs <- x[seq(1, length(x))[which(diff(sign(diff(y)))==2)]+1]
+  troughs_y <- y[!is.na(match(x, troughs))]
+  if (!is.null(threshold)) {
+    troughs <- troughs[troughs_y <= threshold]
+    troughs_y <- troughs_y[troughs_y <= threshold]
+  }
+  return(data.frame(x=troughs, y=troughs_y))
+}
